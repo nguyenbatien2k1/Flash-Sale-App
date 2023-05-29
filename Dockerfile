88b2e8f6c2d69195f829e9e467ac0d74426dcf6f
= <=>
2023-05-29 12:17:04 +0700

    tien update code

--- Dockerfile
+++ Dockerfile
@@ -5,6 +5,7 @@ ENV SHOPIFY_API_KEY=$SHOPIFY_API_KEY
 EXPOSE 8081
 WORKDIR /app
 COPY web .
+RUN ls -al
 RUN npm install
 RUN cd frontend && npm install && npm run build
 CMD ["npm", "run", "serve"]
--- web/config/database/index.js
+++ web/config/database/index.js
@@ -2,11 +2,14 @@ import mongoose from "mongoose";
 
 async function connectDatabase() {
     try {
-        await mongoose.connect('mongodb://localhost:27017/flash-sale');
-        console.log('Connect DB Successfully!')
+        await mongoose.connect('mongodb://mongo/flash-sale')
+            .then(db => console.log('DB is connected to ', db.connection.host))
+            .catch(e => console.log(e))
+        // console.log('Connect DB Successfully!')
 
     } catch (error) {
         console.log('Connect DB Failed!')
+        console.log(error)
     }
 }
 
